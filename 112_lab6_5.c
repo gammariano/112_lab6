1356747c13714a8c077bbbc5eb1b013547cf6e84
Kornrawoot Waramanuchai <kornrawoot.w@ku.th>
2021-03-15 17:43:44 +0700

    Ground

--- 112_lab6_5.c
+++ 112_lab6_5.c
@@ -1,12 +1,18 @@
 #include<stdio.h>
 int main()
 {
-    int i,j,width,height,sum1=0,sum2=0;
+    int i,j,width,height,num1,num2,sum1=0,sum2=0;
     scanf("%d %d",&width,&height);
 	int house[width][height];
 	for(i=0;i<height;i++)
     {
-        scanf("%d",&house[i][j]);
-	   	sum1+=house[i][j];
+        for(j=0;j<width;j++)
+        {
+            scanf("%d",&house[i][j]);
+            sum1+=house[i][j];
+        }
+        num1=sum1;
+        num2=sum1;
     }
+
 }
