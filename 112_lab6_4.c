4b07137497483bede8abdca274d5365caf3d954e
Kornrawoot Waramanuchai <kornrawoot.w@ku.th>
2021-03-15 18:16:11 +0700

    Star School

--- 112_lab6_4.c
+++ 112_lab6_4.c
@@ -1,12 +1,19 @@
 #include<stdio.h>
 int main()
 {
-    int num,max_num=0,min_num=0,i,j,k,m;
+    int num,max_num=0,min_num=0,i,vote1,vote2,m;
     scanf("%d" ,&num);
     int star[num];
     for(i=0;i<num;i++)
     {
         star[i]=0;
     }
+    for(i=0;i<num;i++)
+    {
+        scanf("%d %d" ,&vote1,&vote2);
+        star[vote1-1]=star[vote1-1]+1;
+        star[vote2-1]=star[vote2-1]+1;
+    }
+
     return 0;
 }
