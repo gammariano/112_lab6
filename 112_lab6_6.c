12c8cde54d29433a63d09ef9e2159f04d9d100d4
Kornrawoot Waramanuchai <kornrawoot.w@ku.th>
2021-03-15 19:40:28 +0700

    Drawing Machine

--- 112_lab6_6.c
+++ 112_lab6_6.c
@@ -2,9 +2,18 @@
 #include <string.h>
 int main()
 {
-    int paper,i,j,k;
+    int paper,i,j,k,m=0,n=0;
     char pen[250];
     scanf("%d" ,&paper);
+    char draw[paper][paper];
+    scanf("%s" ,pen);
+    for(i=0;i<paper;i++)
+    {
+        for(j=0;j<paper;j++)
+        {
+            draw[i][j]='.';
+        }
+    }
 
     return 0;
 }
