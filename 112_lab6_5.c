32099d12bfc7344eaa1f67ef714e85fa5fae5d7e
Kornrawoot Waramanuchai <kornrawoot.w@ku.th>
2021-03-15 17:32:17 +0700

    Ground

--- 112_lab6_5.c
+++ 112_lab6_5.c
@@ -3,4 +3,10 @@ int main()
 {
     int i,j,width,height,sum1=0,sum2=0;
     scanf("%d %d",&width,&height);
+	int house[width][height];
+	for(i=0;i<height;i++)
+    {
+        scanf("%d",&house[i][j]);
+	   	sum1+=house[i][j];
+    }
 }
