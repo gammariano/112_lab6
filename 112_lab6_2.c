b8354201dcf4482b4111a4d2152ceeca81d7cd81
Kornrawoot Waramanuchai <kornrawoot.w@ku.th>
2021-03-15 18:00:39 +0700

    เกมทายตัวเลข

--- 112_lab6_2.c
+++ 112_lab6_2.c
@@ -2,5 +2,16 @@
 #include<string.h>
 int main()
 {
-
+    int num,i,count1=0,count2=0;
+    scanf("%d" ,&num);
+    char start_num[num],guess_num[num];
+    scanf(" %[^\n]s",start_num);
+    scanf(" %[^\n]s",guess_num);
+    for(i=0;i<num;i++)
+    {
+        if(start_num[i]==guess_num[i])
+        {
+            count1++;
+        }
+    }
 }
