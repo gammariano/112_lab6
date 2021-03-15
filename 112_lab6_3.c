081072dfbd86792cf9c1ffae353dfd1853980dd3
Kornrawoot Waramanuchai <kornrawoot.w@ku.th>
2021-03-15 19:00:29 +0700

    Segment Tree

--- 112_lab6_3.c
+++ 112_lab6_3.c
@@ -1,13 +1,22 @@
 #include<stdio.h>
 int main()
 {
-    int n,q,i,j,k,z,a,b,count;
+    int n,q,I,i,j,k,z,a,b,count;
     char A[2];
     scanf("%d %d" ,&n,&q);
     int B[n];
-    for(j=0;j<n;j++)
+    for(I=0;I<n;I++)
     {
-
+        B[I]=0;
+    }
+    for(I=0;I<q;I++)
+    {
+        scanf("%s" ,A);
+        if(A[0]=='U')
+        {
+            scanf("%d %d" ,&i,&z);
+            B[i]=z;
+        }
     }
 
 }
